5a9ad5e4d3f78f010ffc8162ab9f1feca58d416d
DimdungPasang <pasangdimdung046@gmail.com>
2022-03-26 10:58:16 +0545

    added entities and basket repositories

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (ShoppingCartItem item in Items)
+                {
+                    totalPrice += item.Quantity * item.Price;
+                }
+
+                return totalPrice;
+            }
+        }
+
+        public List<ShoppingCartItem> Items { get; set; }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+    }
+}
